2fd8d22bbfba99ce199e109fe51fdc84850ce74e
sufyan <mdsufyan7@gmail.com>
2023-08-17 11:23:11 +0530

    add post to API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 import 'semantic-ui-css/semantic.min.css'
+import AddPost from './myapi/AddPost'
 
-import GetPosts from './myapi/GetPosts'
-import GetCats from './myapi/GetCats'
 
 export default function App() {
 
 	return (
 		<div>
-			<GetCats />
+			<AddPost />
 		</div>
 	)
 }
---
+++ src/myapi/AddPost.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+
+export default function AddPost() {
+	const [text, settext] = useState('')
+	const [text2, settext2] = useState('')
+	const [loading, setloading] = useState('')
+
+
+	async function handleSubmit(e) {
+		e.preventDefault()
+		let url = 'https://telugudunia-in.stackstaging.com/wp-json/wp/v2/posts/'
+		let PostData = {
+			status: 'publish',
+			title: text,
+			content: text2
+		}
+		const requestOptions = {
+			method: 'POST',
+			headers: {
+				"Accept": "*/*",
+				"Authorization": 'Basic ' + btoa('test:test@768'),
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(PostData)
+		}
+		setloading('Adding New Post...')
+		const response = await fetch(url, requestOptions)
+		const json = await response.json()
+		setloading('Post added.')
+		settext('')
+		settext2('')
+	}
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+
+				Title: <br />
+				<input
+					type="text"
+					value={text}
+					onChange={(e) => { settext(e.target.value) }}
+				/>
+				<br />
+				Content: <br />
+				<textarea
+					value={text2}
+					onChange={(e) => { settext2(e.target.value) }}
+					cols="30" rows="5"></textarea>
+
+				<button>ADD</button>
+			</form>
+
+			{loading}
+
+		</div>
+	)
+}
+
+
